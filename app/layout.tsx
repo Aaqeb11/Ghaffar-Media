57a303160cdb36ba0d17a96b1406ca122e70200d
Zain <zainhussaini9898@gmail.com>
2024-10-15 21:02:41 +0530

    meta data2

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/logo.png",
-        width: 1200,
-        height: 630,
+        width: 1500,
+        height: 830,
         alt: "Ghaffar Enterprises",
       },
     ],

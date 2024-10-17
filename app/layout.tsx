c67f197c8211a914f941a1ef5d75277c6068bbc6
Aaqeb11 <aaqebahmed@gmail.com>
2024-10-18 03:21:28 +0530

    fix: meta images 13

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import NavBar from "@/components/NavBar";
 import type { Metadata } from "next";
 import "./globals.css";
 
-const baseUrl = "https://ghaffar-media-aaqeb11s-projects.vercel.app/";
+const baseUrl = "https://ghaffarenterprises.com/";
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
